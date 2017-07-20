54854557e65b641f340999ec7d1cb7d1eaa7391a
VrigoXia <1350621044@qq.com>
2017-07-20 16:15:28 +0800

    add
    Added the funtion

--- SOFTWARE/TASK/task.c
+++ SOFTWARE/TASK/task.c
@@ -1,13 +1,13 @@
 /************************************************************
 * 组织名称：
 * 文件名称: K:\单片机相关\电子大赛程序框架\SOFTWARE\TASK\TASK.C
-* 作者:     
-* 版本:    
+* 作者:
+* 版本:
 * 日期:     2017/07/06
-* 描述:
+* 描述:  悬挂运动控制系统基本控制函数
 * 历史修改记录:
 * <作者> <时间> <版本 > <描述>
-* 
+*
 ***********************************************************/
 
 #include "task.h"
@@ -15,4 +15,151 @@
 #include "setup.h"
 #include "../HARDWARE/BSP/USART1.h"
 #include "../HARDWARE/DEVICES/SENSOR/ANGLE/ANGLE.h"
+#include "../HARDWARE/DEVICES/MOTOR/STEP_MOTOR/STEP_MOTOR.h"
+#include <math.h>
+
+#define  BOARD_HEIGHT  100.0f	//板子的高度
+#define  BOARD_WIDTH   50.0f	//板子的宽度
+
+#define LEFT_STEP_MOTOR   0
+#define RIGHT_STEP_MOTOR  1
+
+#define LEFT_WIRE_LENGTH   0 //左边线长
+#define RIGHT_WIRE_LENGTH  1 //右边线长
+
+/*
+  typedef struct //坐标结构体
+{
+
+   float x;
+	 float y;
+
+
+} Coordinate;
+
+typedef struct   //长度结构体
+{  //
+
+   float Length;
+
+
+} Wire;
+static Wire g_StepMotorWireLen[2];  // 电机当前位置记录结构体
+static Coordinate g_TargetCoordinate,g_CurrentCoordinate; // 电机当前位置记录结构体
+
+//利用上面两个结构体一同再构建一个系统信息结构体
+typedef struct
+{
+	  Coordinate g_TargetCoordinate;
+	  Coordinate g_CurrentCoordinate;
+	  Wire g_StepMotorWireLen[2];
+	  enum State systemState;
+}Info;
+
+
+ */
+
+static Info g_MotionSystemInfo; //悬挂运动控制系统信息结构体
+//************************************
+// Method:    isSyetemRunning
+// FullName:  isSyetemRunning
+// Access:    public 
+// Returns:   bit
+// Qualifier: 判断该系统是否正在运行
+// Parameter: void
+//************************************
+bit isSyetemRunning(void)
+{
+	
+}
+//************************************
+// Method:    isSystemStop
+// FullName:  isSystemStop
+// Access:    public 
+// Returns:   bit
+// Qualifier: 判断该系统是否处于静止状态
+// Parameter: void
+//************************************
+bit isSystemStop(void)
+{
+	
+}
+//************************************
+// Method:    startSystem
+// FullName:  startSystem
+// Access:    public 
+// Returns:   bit
+// Qualifier:
+// Parameter: void
+//************************************
+bit startSystem(void)
+{
+	open_StepMotor(STEP_MOTOR_1);
+	open_StepMotor(STEP_MOTOR_2);
+	g_MotionSystemInfo.systemState = RUNNING;
+	
+}
+//************************************
+// Method:    stopSystem
+// FullName:  stopSystem
+// Access:    public 
+// Returns:   bit
+// Qualifier:
+// Parameter: void
+//************************************
+bit stopSystem(void)
+{
+	close_StepMotor(STEP_MOTOR_1);
+	close_StepMotor(STEP_MOTOR_2);
+	g_MotionSystemInfo.systemState = STOP;
+}
+
+//************************************
+// Method:    setTargetCoordinate
+// FullName:  setTargetCoordinate
+// Access:    public 
+// Returns:   bit
+// Qualifier: //设置需要到达的目标坐标
+// Parameter: float x
+// Parameter: float y
+//************************************
+bit setTargetCoordinate(float x, float y)
+{
+	g_MotionSystemInfo.g_TargetCoordinate.x = x;
+	g_MotionSystemInfo.g_TargetCoordinate.y = y;
+	return 1;
+
+}
+//************************************
+// Method:    getCurrentCoordinate
+// FullName:  getCurrentCoordinate
+// Access:    public 
+// Returns:   Coordinate
+// Qualifier: //读取当前坐标，返回一个xy结构体
+// Parameter: u8 motor
+//************************************
+Coordinate getCurrentCoordinate(void)
+{
+
+	return g_MotionSystemInfo.g_CurrentCoordinate;
+}
+
+
+///////////////////！以下为私有函数，外部不能调用！///////////////////////////
+//************************************
+// Method:    setWireLength
+// FullName:  setWireLength
+// Access:    public 
+// Returns:   bit
+// Qualifier: //设置a,b的长度
+// Parameter: u8 wire
+//************************************
+static bit setWireLength(u8 wire)
+{
+	
+}
+static float getWireLength(u8 wire)
+{
+
+}
 
--- SOFTWARE/TASK/task.h
+++ SOFTWARE/TASK/task.h
@@ -13,5 +13,47 @@
 #ifndef _TASK_H_
 #define _TASK_H_
 #include "../HARDWARE/BSP/config.h"
+enum State
+{
+	STOP,RUNNING
+};
+typedef struct //坐标结构体
+{  
+	
+   float x;
+	 float y;
+	  
+	
+} Coordinate;
+
+typedef struct   //长度结构体
+{  //
+	
+   float Length;
+	  
+	
+} Wire;
+static Wire g_StepMotorWireLen[2];  // 电机当前位置记录结构体
+static Coordinate g_TargetCoordinate, g_CurrentCoordinate; // 电机当前位置记录结构体
+
+//利用上面两个结构体一同再构建一个系统信息结构体
+typedef struct  
+{
+	  Coordinate g_TargetCoordinate;
+	  Coordinate g_CurrentCoordinate;
+	  Wire g_StepMotorWireLen[2];
+	  enum State systemState;
+}Info;
+	
+
+
+extern Coordinate getCurrentCoordinate(void);
+extern bit setTargetCoordinate(float x, float y);
+static bit setWireLength(u8 wire);
+bit isSyetemRunning(void);
+bit isSystemStop(void);
+bit startSystem(void);
+bit stopSystem(void);
+
 
 #endif
